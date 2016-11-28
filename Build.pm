a391cf42dd628593b50b92fb1f517e8a8906291c
spebern <bernhard@specht.net>
2016-11-29 00:17:54 +0100

    modify prefix dir

--- Build.pm
+++ Build.pm
@@ -5,7 +5,7 @@ use LibraryMake;
 class Build is Panda::Builder {
     method build($workdir) {
         my Str $os = qx[uname -s 2>/dev/null || echo not];
-        my $prefix_dir = IO::Path.new('./lib').absolute;
+        my $prefix_dir = IO::Path.new('.').absolute;
         if chomp($os) ~~ "FreeBSD" {
             shell("cd stub; ./configure --prefix=$prefix_dir; gmake install");
         }
